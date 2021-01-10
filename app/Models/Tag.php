11ec326a5719b1fac860ef2f758ab2afbe0969eb
Olari George <olarigeorge1998@yahoo.com>
2021-01-10 19:48:25 +0200

    different changes in models

--- app/Models/Tag.php
+++ app/Models/Tag.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Tag extends Model
 {
+    use \Backpack\CRUD\app\Models\Traits\CrudTrait;
     use SoftDeletes;
 
     /**
--- routes/backpack/custom.php
+++ routes/backpack/custom.php
@@ -14,4 +14,7 @@ Route::group([
     ),
     'namespace'  => 'App\Http\Controllers\Admin',
 ], function () { // custom admin routes
+    Route::crud('article', 'ArticleCrudController');
+    Route::crud('category', 'CategoryCrudController');
+    Route::crud('tag', 'TagCrudController');
 }); // this should be the absolute last line of this file
